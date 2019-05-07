b5267e70617599c4bdee819583642dcdf6b40f91
Sean Seung Won Lee <swdev1202@gmail.com>
2019-05-07 13:01:47 -0700

    tttt

--- preprocessing.py
+++ preprocessing.py
@@ -287,6 +287,7 @@ class BatchGenerator(Sequence):
                 for obj in all_objs:
                     print(image_name + " : " + obj['name'])
                     if obj['xmax'] > obj['xmin'] and obj['ymax'] > obj['ymin']:
+                        print(obj['xmax'], obj['xmin'], obj['ymax'], obj['ymin'])
                         cv2.rectangle(img[:,:,::-1], (obj['xmin'],obj['ymin']), (obj['xmax'],obj['ymax']), (255,0,0), 3)
                         cv2.putText(img[:,:,::-1], obj['name'], 
                                     (obj['xmin']+2, obj['ymin']+12),
