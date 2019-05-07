285f5e15bf6adfafded3fa1b52a87d9e406a0dfd
Sean Seung Won Lee <swdev1202@gmail.com>
2019-05-07 12:43:11 -0700

    try3

--- preprocessing.py
+++ preprocessing.py
@@ -219,6 +219,7 @@ class BatchGenerator(Sequence):
         for train_instance in self.images[l_bound:r_bound]:
             image_name = train_instance['filename']
             img = cv2.imread(image_name)[:,:,0:2]
+            print(image_name)
 
             if img is None: print('Cannot find ', image_name)
 
@@ -284,11 +285,11 @@ class BatchGenerator(Sequence):
             else:
                 # plot image and bounding boxes for sanity check
                 for obj in all_objs:
-                    print("here")
+                    print(obj['name'])
                     if obj['xmax'] > obj['xmin'] and obj['ymax'] > obj['ymin']:
                         cv2.rectangle(img[:,:,::-1], (obj['xmin'],obj['ymin']), (obj['xmax'],obj['ymax']), (255,0,0), 3)
                         cv2.putText(img[:,:,::-1], obj['name'], 
-                                    (obj['xmin']+2, obj['ymin']+12), 
+                                    (obj['xmin']+2, obj['ymin']+12),
                                     0, 1.2e-3 * img.shape[0], 
                                     (0,255,0), 2)
                         # debug code
