ec270850d7d437d4e21721b8c40faf1ea1c9981d
Deepak Talwar <deepaktalwardt>
2019-06-09 23:50:08 +0530

    Added inference code

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ from yolo3d_model import infer_image
 
 weights = "../apollo_yolo3d_1.h5"
 # image_path = "../dataset/val/1557021023820550.jpg"
-image_path = "../dataset_front/val/result_9055_6_192.png"
+image_path = "../dataset_front/val/result_9048_1_233.png"
 output_path = "output/"
 
 infer_image(weights, image_path, output_path, save_image=True)
